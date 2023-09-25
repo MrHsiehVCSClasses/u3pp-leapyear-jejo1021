90901fd64b55e50829955e904e242af4f14f74f7
jeremy john <jeremyjohn1021@gmail.com>
2023-09-25 12:02:09 -0700

    done

--- src/main/java/u3pp/LeapYear.java
+++ src/main/java/u3pp/LeapYear.java
@@ -8,10 +8,36 @@ package u3pp;
  */
 
 public class LeapYear {
+	
 	/**
 	 * YOUR COMMENTS HERE
+	 * 
 	 */
+	
+	
 	public static boolean isLeapYear(int year) {
 		/* YOUR CODE HERE */
+		
+		
+		if(year < 1582) {
+			return false;
+		}
+		
+		else if(!(year % 2 == 0)) {
+			return false;
+		}
+		
+		else if(year >= 1582 && year % 4 ==0 &&!(year % 100 == 0)) {
+			return true;
+		}
+		else if(year >= 1582 && year % 100 == 0 && year % 400 == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+		
+		
 	}
 }
--- src/main/java/u3pp/Main.java
+++ src/main/java/u3pp/Main.java
@@ -2,7 +2,7 @@ package u3pp;
 
 import java.util.Scanner;
 
-/**
+/**S
  * YOUR COMMENTS HERE
  * 
  * @author YOUR NAME HERE
@@ -13,15 +13,39 @@ class Main {
 	public static void main(String[] args) {
 
 		//YOUR CODE HERE
-
-		System.out.print("Enter a year (or enter STOP to end): ");
-		String input = sc.nextLine();
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a year (or enter STOP to end): ");
+		String input = sc.nextLine();		
+		//int year = Integer.parseInt(input);
+		//boolean check = LeapYear.isLeapYear(year);
+		
+		
 		while (!input.toUpperCase().equals("STOP")) {
 			/* YOUR CODE HERE */
-			System.out.print("Enter a year (or enter STOP to end): ");
+			int year = Integer.parseInt(input);
+			boolean check = LeapYear.isLeapYear(year);
+			
+			if(check == true) {
+				System.out.println(input + " is a leap year");
+				
+			}
+			else if(check == false) {
+				System.out.println(year + " is not a leap year");
+			}
+			
+
+			System.out.println("Enter a year (or enter STOP to end): ");
+			
 			//YOUR CODE HERE
+			
+
+			//if(input == "STOP") {
+			//	sc.close();
+				
+			input = sc.nextLine();
+			//}
 		}
 		//YOUR CODE HERE
+		sc.close();
 	}
 }
